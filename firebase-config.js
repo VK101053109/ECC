a2da4eed763f9851098a6f4e66a59c49c366e1bc
Vaishnav10 <116233978+VK101053109@users.noreply.github.com>
2026-06-29 16:25:40 +0000

    Delete All Files

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.ECC_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBf2AoirA_3m9xO1MgCPV5cZ459jePhgzU",
-    authDomain: "ecc1-13809.firebaseapp.com",
-    projectId: "ecc1-13809",
-    storageBucket: "ecc1-13809.firebasestorage.app",
-    messagingSenderId: "562600937128",
-    appId: "1:562600937128:web:ea3e7987f561b6cad6a49d",
-    measurementId: "G-V121D0QTJW"
-};
